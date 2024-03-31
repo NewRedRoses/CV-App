02b569693dd268f19311c31a5c9bc497148f0c1d
Mark A. <mark001616@gmail.com>
2024-03-31 18:02:53 -0400

    Organize component files

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import "/src/styles/Header.css";
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="header-text"> CV Maker</div>
+    </div>
+  );
+}
